d5fb3a15e7621c5865343a2bfa3d511e4059119e
Samantha Garcia <sgarcia22@ufl.edu>
2019-04-04 14:37:08 -0400

    Project

---
+++ Assets/Scripts/RPS/LeapMotionController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+//Hand Controller for Rock Paper Scissors
+public class LeapMotionController : MonoBehaviour {
+
+    Controller controller;
+    Frame frame;
+    List<Hand> hands;
+    public bool rock = false, paper = false, scissors = false;
+
+	void Start () {
+        controller = new Controller();
+        frame = controller.Frame();
+        hands = frame.Hands;
+        Debug.Log("Starting");
+
+	}
+	
+	void Update () {
+        frame = controller.Frame();
+        hands = frame.Hands;
+        Hand primaryHand = null;
+        //Get the first hand on the scene and set it as the primary hand
+        if (frame.Hands.Count > 0)
+        {
+            Debug.Log("Entering");
+            primaryHand = frame.Hands[0];
+            //Detect Rock Paper or Scissors
+            //Get all of the fingers
+            Finger thumb, index, middle, ring, pinky; 
+            thumb = primaryHand.Fingers[0];
+            index = primaryHand.Fingers[1];
+            middle = primaryHand.Fingers[2];
+            ring = primaryHand.Fingers[3];
+            pinky = primaryHand.Fingers[4];
+
+            //Rock
+            if (!thumb.IsExtended && !index.IsExtended && !middle.IsExtended && !ring.IsExtended && !pinky.IsExtended)
+                rock = true;
+            else
+                rock = false;
+            
+            //Paper
+            if (thumb.IsExtended && index.IsExtended && middle.IsExtended && ring.IsExtended && pinky.IsExtended)
+                paper = true;
+            else
+                paper = false;
+            //Scissors
+            if (!thumb.IsExtended && index.IsExtended && middle.IsExtended && !ring.IsExtended && !pinky.IsExtended)
+                scissors = true;
+            else
+                scissors = false;
+
+
+            /*if (rock)
+                Debug.Log("Rock");
+            if (paper)
+                Debug.Log("Paper");
+            if (scissors)
+                Debug.Log("Scissors");
+                */
+        }
+
+        //Set booleans to false on frame end
+        /*rock = false;
+        paper = false;
+        scissors = false;
+        */
+    }
+}
---
+++ Assets/Scripts/RPS/RPSGM.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RPSGM : MonoBehaviour {
+
+    public LeapMotionController LPC;
+    bool compRock = false, compPaper = false, compScissors = false, userRock = false, userPaper = false, userScissors = false;
+    public GameObject roc, pap, sci;
+    public Text UI;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("RPS", 0f, 10f);
+
+    }
+	
+    private void RPS()
+    {
+        roc.SetActive(false);
+        pap.SetActive(false);
+        sci.SetActive(false);
+        //Pick RPS
+        int pick = Random.Range(0, 3);
+        if (pick == 0) compRock = true;
+        else if (pick == 1) compPaper = true;
+        else compScissors = true;
+        StartCoroutine("countdown");
+        //Countdown
+
+    }
+
+    IEnumerator countdown ()
+    {
+        UI.text = "3";
+        yield return new WaitForSeconds(1);
+        UI.text = "2";
+        yield return new WaitForSeconds(1);
+        UI.text = "1";
+        yield return new WaitForSeconds(1);
+
+        if (compRock)
+            roc.SetActive(true);
+        if (compPaper)
+            pap.SetActive(true);
+        if (compScissors)
+            sci.SetActive(true);
+
+
+        if (userRock)
+            Debug.Log("Rock");
+        if (userPaper)
+            Debug.Log("Paper");
+        if (userScissors)
+            Debug.Log("Scissors");
+
+        //Win Conditions
+        if (compRock && userRock || compScissors && userScissors || userPaper && compPaper)
+            UI.text = "Tie";
+        else if (compRock && userPaper || compPaper && userScissors || compScissors && userRock)
+            UI.text = "Win";
+        else
+            UI.text = "Loss";
+
+
+        yield return new WaitForSeconds(3);
+        compPaper = false; compScissors = false; compRock = false;
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        userRock = LPC.rock; userPaper = LPC.paper; userScissors = LPC.scissors;
+    }
+}
